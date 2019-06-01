d7edf3a026b7b0595bc3d449e2629f39eee1ee28
revjatto <revjatto@yahoo.co.uk>
2019-06-01 01:16:47 +0100

    Added title to the model

--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=500)
-
+    title = models.CharField(max_length=100, null=True)
+    
     def __self__(self):
-        return self.summary
+        return self.title
         
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME':'portfoliodb',
-       'USER': 'postgres',
-       'PASSWORD': 'u24tuc9r',
-       'HOST': 'localhost',
-       'PORT': '5432',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       # 'ENGINE': 'django.db.backends.postgresql',
+       # 'NAME':'portfoliodb',
+       # 'USER': 'postgres',
+       # 'PASSWORD': 'u24tuc9r',
+       # 'HOST': 'localhost',
+       # 'PORT': '5432',
     }
 }
 
